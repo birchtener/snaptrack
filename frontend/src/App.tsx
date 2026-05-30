3facbd78bcdcd8ef2ce32400e9f1e697d78ed674
birchtener <birchardtheo.tener@carsu.edu.ph>
2026-05-31 00:52:27 +0800

    feat: enhance routing logic with authentication checks and improve navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,37 @@ import AuthGuard from "@/pages/auth/components/AuthGuard";
 import { LoginForm } from "./pages/auth/components/LoginForm";
 import { SignupForm } from "./pages/auth/components/SignupForm";
 import { Button } from "@base-ui/react";
-import { useClerk } from "@clerk/react";
+import { useClerk, useAuth } from "@clerk/react";
 function App() {
   const { signOut } = useClerk();
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="snaptrack-theme">
       <BrowserRouter>
         <Routes>
+          <Route
+            path="/"
+            element={
+              <Navigate
+                to={isSignedIn ? "/app/organizations" : "/login"}
+                replace
+              />
+            }
+          />
+
           <Route element={<AuthPage />}>
             <Route path="/login" element={<LoginForm />} />
             <Route path="/signup" element={<SignupForm />} />
           </Route>
+
           <Route element={<AuthGuard />}>
             <Route
-              index
+              path="/app/organizations"
               element={
                 <div>
                   <Button onClick={() => signOut({ redirectUrl: "/" })}>
@@ -29,7 +45,16 @@ function App() {
               }
             />
           </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
+
+          <Route
+            path="*"
+            element={
+              <Navigate
+                to={isSignedIn ? "/app/organizations" : "/login"}
+                replace
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
